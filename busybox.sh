184aa0d150b0106bdea9dcfbe019371c419733a0
VR-25 <28943176+VR-25@users.noreply.github.com>
2024-12-15 09:52:06 +0100

    Update busybox config

--- busybox.sh
+++ busybox.sh
@@ -1,5 +1,5 @@
 # Busybox Setup
-# Copyright 2019-2023, VR25
+# Copyright 2019-2024, VR-25 @GitHub
 # License: GPLv3+
 #
 # Usage: . $0
@@ -7,16 +7,13 @@
 
 bin_dir=/data/adb/vr25/bin
 busybox_dir=/dev/.vr25/busybox
-magisk_busybox="/data/adb/ksu/bin/busybox /data/adb/magisk/busybox"
+magisk_busybox="$(ls /data/adb/*/bin/busybox /data/adb/magisk/busybox 2>/dev/null || :)"
 
 [ -x $busybox_dir/ls ] || {
   mkdir -p $busybox_dir
   chmod 0755 $busybox_dir $bin_dir/busybox 2>/dev/null || :
   for f in $bin_dir/busybox $magisk_busybox /system/*bin/busybox*; do
-    [ ! -f $f ] || {
-      $f --install -s $busybox_dir/
-      break
-    }
+    [ -x $f ] && eval $f --install -s $busybox_dir/ && break || :
   done
   [ -x $busybox_dir/ls ] || {
     echo "Install busybox or simply place it in $bin_dir/"
